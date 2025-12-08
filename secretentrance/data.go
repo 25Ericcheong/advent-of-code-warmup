b4f60d26d1b43dfc87cdcbc70648dc4c49dd5a3d
25Ericcheong <ericcheongkaikit@gmail.com>
2025-12-09 07:31:33 +0800

    refactor: Simplify code into single file

--- secretentrance/data.go
+++ secretentrance/data.go
@@ -1,5 +1,41 @@
 package secretentrance
 
+type Dial struct {
+	Count           int
+	PasswordCounter int
+}
+
+const minDialCount = 0
+const maxDialCount = 99
+
+const normalizeCount = 100
+
+func (d *Dial) TurnLeft(number int) {
+	d.Count = -number
+
+	if d.Count < minDialCount {
+		d.Count += normalizeCount
+	}
+
+	d.plusPasswordCounterIfNeeded()
+}
+
+func (d *Dial) TurnRight(number int) {
+	d.Count += number
+
+	if d.Count > maxDialCount {
+		d.Count %= normalizeCount
+	}
+
+	d.plusPasswordCounterIfNeeded()
+}
+
+func (d *Dial) plusPasswordCounterIfNeeded() {
+	if d.Count == minDialCount {
+		d.PasswordCounter += 1
+	}
+}
+
 func LoadData() {
 
 }
